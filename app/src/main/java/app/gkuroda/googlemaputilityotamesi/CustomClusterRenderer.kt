fff8294d1a0a48d0f806242271cb28af73aa2aeb
kuroda <kuroda@appirits.com>
2021-06-25 17:28:14 +0900

    クラスタのアイコンを差し替える

--- app/src/main/java/app/gkuroda/googlemaputilityotamesi/CustomClusterRenderer.kt
+++ app/src/main/java/app/gkuroda/googlemaputilityotamesi/CustomClusterRenderer.kt
@@ -1,12 +1,17 @@
 package app.gkuroda.googlemaputilityotamesi
 
 import android.content.Context
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
+import com.google.maps.android.ui.IconGenerator
 
 class CustomClusterRenderer(
     val context: Context?,
@@ -31,5 +36,61 @@ class CustomClusterRenderer(
             markerOptions.icon(bitmapDescriptor)
         }
 
+    }
+
+    override fun onBeforeClusterRendered(
+        cluster: Cluster<MapItem>,
+        markerOptions: MarkerOptions
+    ) {
+        val icon = makeIcon(cluster.size)
+        if (icon != null) markerOptions.icon(icon)
+    }
+
+    override fun onClusterUpdated(cluster: Cluster<MapItem>, marker: Marker) {
+        val icon = makeIcon(cluster.size)
+        if (icon != null) marker.setIcon(icon)
+    }
+
+    /** クラスタのアイコンを生成する */
+    private fun makeIcon(clusterSize: Int): BitmapDescriptor? {
+        var result: BitmapDescriptor? = null
+        val clusterIconGenerator = IconGenerator(context)
+
+        if (context != null) {
+            //確定nullだと面倒なので一番小さいものを詰めておく
+            var clusterIcon = ContextCompat.getDrawable(context, R.drawable.cluster_a)
+
+            //アイコンに表示するテキスト
+            var clusterText = clusterSize.toString()
+
+            //クラスタのサイズに合わせてアイコンを変更する
+            when (clusterSize) {
+                in 0 until 10 -> {
+                    //それぞれにあったアイコンを指定
+                    clusterIcon = ContextCompat.getDrawable(context, R.drawable.cluster_a)
+                    clusterText = clusterSize.toString()
+                }
+                in 10 until 100 -> {
+                    clusterIcon = ContextCompat.getDrawable(context, R.drawable.cluster_b)
+                    clusterText = "${(clusterSize / 10)}0+"
+                }
+                in 100 until 1000 -> {
+                    clusterIcon = ContextCompat.getDrawable(context, R.drawable.cluster_c)
+                    clusterText = "${(clusterSize / 100)}00+"
+                }
+                in 1000 until 10000 -> {
+                    clusterIcon = ContextCompat.getDrawable(context, R.drawable.cluster_d)
+                    clusterText = "${(clusterSize / 1000)}000+"
+                }
+            }
+
+            // アイコン用を生成するクラスにバックグラウンド画像を渡す
+            clusterIconGenerator.setBackground(clusterIcon)
+
+            // クラスタ内に表示するテキストを設定してBitmapDescriptorを生成する
+            result = BitmapDescriptorFactory.fromBitmap(clusterIconGenerator.makeIcon(clusterText))
+
+        }
+        return result
     }
 }
